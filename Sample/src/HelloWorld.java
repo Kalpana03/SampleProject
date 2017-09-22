b47846b5dae277512deccf668e6a47b5551fec48
Kalpana03 <31638383+Kalpana03@users.noreply.github.com>
2017-09-22 12:28:03 -0400

    committing code.

---
+++ Sample/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+	public void Hello()
+	{
+		System.out.println("Welcome to Jenkins");
+
+	}
+	public static void main (String[] args)
+	{
+		HelloWorld hello = new HelloWorld();
+		hello.Hello();
+	}
+
+}
